adffb52b32751ada222a74436283a8c4ecb230f9
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 19:40:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,15 +10,3 @@ def my_each(yield_arg) # put argument(s) here
 end
 
 my_each(array_yield) {|container| "So I think #{container} is a piece of shit"}
-
-# def hello(array)
-#   i = 0
-#   collection = []
-#   while i < array.length
-#     collection << yield(array[i])
-#     i += 1
-#   end
-#   collection
-# end
-
-# hello(list) {|i| "Hello #{i} what's up?"}

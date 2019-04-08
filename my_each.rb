c3b061d8e8fc1e3e42860afede176a9c1a5c5e1c
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 19:27:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,13 @@
 def my_each(yield_arg) # put argument(s) here
-  # code here
+  counter = 0
   
+  while counter < yield_arg.length
+    yield(yield_arg[counter])
+    counter += 1
+  end
 end
+
+list = ["ab", "cd", "ef"]
+
+
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}

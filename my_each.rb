b7a3f2545a9564674785b3059ce565a1ab5c1e09
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 19:35:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,11 @@ array_yield = ["Apple", "Mango", "Orange"]
 
 def my_each(yield_arg) # put argument(s) here
   counter = 0
-  
   while counter < yield_arg.length
     yield(yield_arg[counter])
     counter += 1
   end
 end
-
 
 my_each(array_yield) {|container| puts "So I think #{container} is a piece of shit"}
 
